2d5d77a1c8fcc0aa59b4896df9fd7641ac147b47
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-02-09 19:35:31 +0100

    chore: integrate atlas as DB

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,18 +1,28 @@
-const express = require ('express');
-const { createUser, getAllUsers, getaSingleUser, loginUser, logout, handleRefreshToken, deleteaUser, updatePassword, resetPassword, forgotPasswordToken } = require('../controllers/userCtrl');
-const { isAdmin, authMiddleware } = require('../middlewares/authMiddleware');
+const express = require("express");
+const {
+  createUser,
+  getAllUsers,
+  getaSingleUser,
+  loginUser,
+  logout,
+  handleRefreshToken,
+  deleteaUser,
+  updatePassword,
+  resetPassword,
+  forgotPasswordToken,
+} = require("../controllers/userCtrl");
+const { isAdmin, authMiddleware } = require("../middlewares/authMiddleware");
 const router = express.Router();
 
-router.post('/register' , createUser);
-router.get ('/all-users', getAllUsers);
-router.post('/login', loginUser);
-router.get('/refresh', handleRefreshToken)
-router.get('/logout', logout);
-router.get ('/:id',authMiddleware, isAdmin, getaSingleUser );
-router.delete('/:id', deleteaUser);
-router.post ('/forgot-password-token', forgotPasswordToken);
-router.put ('/reset-password/:token', resetPassword);
-router.put('/password', authMiddleware, updatePassword);
-
+router.post("/register", createUser);
+router.get("/all-users", getAllUsers);
+router.post("/login", loginUser);
+router.get("/refresh", handleRefreshToken);
+router.get("/logout", logout);
+router.get("/:id", authMiddleware, isAdmin, getaSingleUser);
+router.delete("/:id", deleteaUser);
+router.post("/forgot-password-token", forgotPasswordToken);
+router.put("/reset-password/:token", resetPassword);
+router.put("/password", authMiddleware, updatePassword);
 
 module.exports = router;
