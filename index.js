d5ed82929ccf3145c590ead4dc95679004f1c389
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-02-11 14:26:05 +0100

    fix: fix db connection issue

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
-const express = require ('express');
-const authRouter = require ('./routes/authRoutes');
-const kycRoute = require ('./routes/kycRoute');
-const { notFound, errorHandler } = require('./middlewares/errorHandler');
-const dotenv = require('dotenv').config();
+const express = require("express");
+const authRouter = require("./routes/authRoutes");
+const kycRoute = require("./routes/kycRoute");
+const { notFound, errorHandler } = require("./middlewares/errorHandler");
+const dotenv = require("dotenv").config({ path: "./config.env" });
 const app = express();
-const dbConnect = require ('./config/dbConnect');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const morgan = require('morgan')
-const PORT = process.env.PORT;
+const dbConnect = require("./config/dbConnect");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const PORT = process.env.PORT || 3000;
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+// Connect DB
 dbConnect();
 
-
-
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(cookieParser())
-
-app.use('/api/user', authRouter);
-app.use('/api/kyc', kycRoute)
-
+app.use("/api/user", authRouter);
+app.use("/api/kyc", kycRoute);
 
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(PORT, () => {
-    console.log(`server is listening at ${PORT}`)
+  console.log(`server is listening at ${PORT}`);
 });
