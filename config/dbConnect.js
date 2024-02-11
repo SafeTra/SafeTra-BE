c86f4bf3acea0ece4bf985d4edce29631da54aa2
Anosike Jayson <kayceeanosike1@gmail.com>
2024-02-11 10:12:49 +0100

    transaction features

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,23 +1,13 @@
-const { default: mongoose } = require('mongoose');
+const { default: mongoose } = require("mongoose")
 
 const dbConnect = () => {
-  try {
-    // const conn = mongoose.connect(process.env.MONGODB_URL);
-    // console.log("CONNECTED TO DATABASE SUCCESSFULLY");
-    const DB = process.env.MONGO_ATLAS.replace(
-      '<password>',
-      process.env.DATABASE_PASSWORD
-    );
-
-    mongoose
-      .connect(DB, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() => console.log('DB connection successful'));
-  } catch (error) {
-    console.log('DATBASE ERROR!!');
-  }
-};
+    try {
+        const conn = mongoose.connect('mongodb://127.0.0.1:27017/safeTra+');
+        console.log('CONNECTED TO DATABASE SUCCESSFULLY');
+    } catch (error) {
+        console.log("DATABASE ERROR!!");
+    }
+    
+}
 
 module.exports = dbConnect;
