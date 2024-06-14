55478efb6f30e3fb4165ee8f7126a99918df1e3e
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-14 13:32:05 +0100

    setup controllers for KYC validations

--- controllers/kycCtrl.js
+++ controllers/kycCtrl.js
@@ -37,31 +37,62 @@ const handleKYC = asyncHandler (async (req, res) => {
 });
 
 
+const validateIdDocument = asyncHandler (async (documentId, idType, userKyc) => {
+    /*
+        Todo:
+        Send validation credentials to third party
+        for idType validation.
+
+        userKyc's isIdVerified will be set to true on successful 
+        validation. 
+        
+        Verification response details will be stored in idCredentials.
+    */
+   
+});
+
+const validateEmail = asyncHandler (async (emailAddress) => {
+    /*
+        Todo:
+        Send verification email with link to verification page on FE or OTP.
+    
+        userKyc's isEmailVerified will be set to true on successful 
+        verification. 
+    */
+
+});
+
+
+const genrateMobileOtp = asyncHandler (async (mobileNumber) => {
+    /*
+        Todo:
+        Send OTP to number via third-party.
+    */
+});
+
+
+const validateMobile = asyncHandler (async (mobileNumber) => {
+    /*
+        Todo:
+        userKyc's isMobileVerified will be set to true on successful 
+        OTP verification via third-party. 
+    */
+});
+
+
 const updateKYC = asyncHandler (async (req, res) => {
     const {_id }= req.user;
     validateMongodbid(_id);  // Might not be necessary
-    if (!req.file) {
-        throw new Error('No file Uploaded');
-    };
 
     try {
-        const kyc = await kyc.findByIdAndUpdate(
-            _id,
-            {
-                firstname: req.body.firstname,
-                lastname: req.body.lastname,
-                address: req.body.address,
-                photo: req.file.filename,
-            },
-            {
-                new: true,
-            }
-        );
-        res.json({ kyc });
-    } catch (error) {
-        throw new Error ('Error Updating KYC');
-    }
+        // Find user's KYC instance
 
+        // Detect KYC field to be validated 
+        // and run respective validation callbacks
+        
+    } catch (error) {
+
+    }
    
 });
 
