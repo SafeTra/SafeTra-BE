38f32482b847a099493311b30edc9309e83f8985
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-14 14:56:59 +0100

    added KYC routes to router

--- routes/kycRoute.js
+++ routes/kycRoute.js
@@ -2,12 +2,16 @@ const express = require ('express');
 const router = express.Router();
 const multer = require ('multer');
 const { authMiddleware } = require('../middlewares/authMiddleware');
-const { handleKYC } = require('../controllers/kycCtrl');
+const { 
+    kycUpdate,
+    kycVerification,
+ } = require('../controllers/kycCtrl');
 
 const upload = multer ({dest: 'uploads/'});
 
 
-router.post('/update-KYC/:id', authMiddleware, upload.single('photo'), handleKYC);
+router.post('/verify-KYC/:id', authMiddleware, kycVerification);
+router.post('/update-KYC/:id', authMiddleware, kycUpdate);
 
 
 module.exports = router;
