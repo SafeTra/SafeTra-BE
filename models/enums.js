3d5b76be7bee480ba40d730918d58b08690bb83e
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-14 05:49:35 +0100

    added new model enums

--- models/enums.js
+++ models/enums.js
@@ -3,6 +3,22 @@ const ROLES = {
     USER: "user"
 };
 
+const TRANSACTION_STATUS = {
+    INITIATED: "initiated",
+    COMPLETED: "completed",
+    VERIFIED: "verified",
+    PENDING: "pending",
+}
+
+const ID_TYPE = {
+    BVN: 'BVN',
+    NIN: 'NIN_V2',
+    VOTER_ID: 'VOTER_ID',
+    DRIVERS_LICENSE: 'DRIVERS_LICENSE',
+}
+
 module.exports = {
-    ROLES
+    ROLES,
+    TRANSACTION_STATUS,
+    ID_TYPE
 }
