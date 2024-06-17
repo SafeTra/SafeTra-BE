2f3166644ab8dee275076ecddf4c57887d58abd8
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-17 11:58:17 +0100

    removed escrow from users

--- modules/wallets/models.js
+++ modules/wallets/models.js
@@ -3,10 +3,6 @@ const mongoose = require('mongoose');
 
 let walletSchema = new mongoose.Schema(
   {
-    // id: {      Id provided by the virtual account provider/holder
-    //     type: String,
-    //     required: true,
-    // },
     user_id: {
       type: String,
       default: null,
@@ -23,13 +19,7 @@ let walletSchema = new mongoose.Schema(
     totalRevenue: {
         type: Number,
         default: 0,
-    },
-    // wallet_holder:{    Provider/Holder of the virtual account
-    //   type: mongoose.Schema.Types.ObjectId,
-    //   ref: 'Kyc',
-    //   default: null,
-    //   required: false,
-    // },
+    }
   },
   {
     timestamps: true,
