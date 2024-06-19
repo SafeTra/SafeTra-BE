a87542b4ed66b123f585420b2627de218e955d71
JhimmieC137 <toluwalope.david@gmail.com>
2024-06-19 12:29:40 +0100

    completed merge

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fileUpload = require('express-fileupload');
 const morgan = require('morgan');
 const express = require('express');
 const session = require ("express-session");
@@ -12,13 +13,9 @@ const dbConnect = require('./config/dbConnect');
 const { authRouter, route: authPath } = require('./modules/auth/routes');
 const { userRouter, route: userPath } = require('./modules/users/routes');
 const { transactionRouter, route: transactionPath } = require('./modules/transactions/routes');
-<<<<<<< HEAD
 const { itemRouter, route: itemPath } = require('./modules/items/routes');
 const { fileRouter, route: filePath } = require('./modules/files/routes');
-const fileUpload = require('express-fileupload');
-=======
 const { referralRouter, route: referralPath } = require('./modules/referrals/routes');
->>>>>>> 90d542be1d1e066fe10a53d5b4105dc2dbf68120
 
 const app = express();
 
@@ -62,22 +59,14 @@ const apiVersion = '/api/v1'
 app.use(apiVersion + userPath, userRouter);
 app.use(apiVersion + authPath, authRouter);
 app.use(apiVersion + transactionPath, transactionRouter);
-<<<<<<< HEAD
 app.use(apiVersion + itemPath, itemRouter);
 app.use(apiVersion + filePath, fileRouter);
-=======
 app.use(apiVersion + referralPath, referralRouter);
->>>>>>> 90d542be1d1e066fe10a53d5b4105dc2dbf68120
 
 
 app.use(notFound);
 app.use(errorHandler);
 app.use(fileUpload())
-
-// console.log(
-//   `https://safetra.s3.eu-west-2.amazonaws.com/b32d8371-eb5d-4d04-8310-d5115a8ff283__1718791583352.jpg` == `https://safetra.s3.eu-west-2.amanzonaws.com/b32d8371-eb5d-4d04-8310-d5115a8ff283__1718791583352.jpg`
-// )
-
 
 app.listen(PORT, () => {
   console.log(`server is listening at ${PORT}`);
